9c3b086a86a39f9878101eed6a2a65f04d61d97f
Mark Murphy <murphy.mark@kls-00049.local>
2020-04-09 21:48:46 -0300

    Add CNAME file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-const isProduction = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  assetPrefix: isProduction ? '/silver-adventure' : '',
   env: {
     API_ACCESS_TOKEN: process.env.API_ACCESS_TOKEN,
   },
